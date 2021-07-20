acd8a958c53411693029ffbf8c6e998a750f5e85
Lazuardy Khatulistiwa <dimazkid@gmail.com>
2021-07-20 15:19:45 +0700

    fixed migrate & added setup configuration

--- app/database/migrate.py
+++ app/database/migrate.py
@@ -1,15 +1,17 @@
 import pymongo
+from app import app
+app.config.from_pyfile('settings.cfg', silent=True)
 
 class Migrate:
     def __init__(self):
-        dbclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        db = dbclient["vips"]
+        mongocon = app.config.get('MONGO_CON')
+        dbclient = pymongo.MongoClient(mongocon)
+        db = dbclient[app.config.get('DATABASE')]
         col_users = db["users"]
         dict_users = [
             {"name":"Taufik Hidayat", "country_of_origin":"Indonesia", "eta":"2021-07-20 09:21:20", "photo":"https://staticg.sportskeeda.com/wp-content/uploads/2012/02/taufik-hidayat.jpg", "arrived":False, "attributes":["red jacket", "blue jeans"]}
         ]
-        insert_user = col_users.insert_many(dict_users)
-        userid_inserted = insert_user.inserted_ids
+        col_users.insert_many(dict_users)
 
         print(dbclient.list_database_names())
         print(db.list_collection_names())
